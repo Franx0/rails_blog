0eb0db8c57032db77ce38a3679f06629186f391b
Franx0 <francisco.moya.martinez@gmail.com>
2014-12-12 13:37:02 +0100

    now can write saved comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,12 @@
 class CommentsController < ApplicationController
-    
+
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(params[:comment].permit(:name, :body))
+        if @comment.save
+            redirect_to post_path(@post)
+        else 
+            alert('Your comment has not been created')
+        end
+    end
 end
