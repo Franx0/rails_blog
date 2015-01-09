0011ee69a6b50fe23a2645394219ed7941e94df8
Franx0 <francisco.moya.martinez@gmail.com>
2015-01-09 20:36:06 +0100

    order in posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,9 @@ class PostsController < ApplicationController
 
     def index
         if params[:search]
-            @post = Post.search(params[:search]).order("created_at DESC")
+            @post = Post.search(params[:search])
         else
-            @post = Post.all.order("created_at DESC")
+            @post = Post.all
         end
     end
 
